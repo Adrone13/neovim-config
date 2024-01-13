1a4fd68140a525f696b76e3b497fbbc219329c10
aleksandr.s <alex@Aleksandrs-MacBook-Pro.local>
2024-01-14 01:16:52 +0200

    refactor

---
+++ lua/adrone13/formatting.lua
@@ -0,0 +1,28 @@
+-- Linter configuration
+function ConfigConform()
+  local conform = require("conform")
+  local format_on_save_config = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_fallback = true,
+    timeout_ms = 500,
+    -- async = false, -- If the buffer is modified before the formatter completes, the formatting will be discarded.
+  }
+
+
+  conform.setup({
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      go = { "goimports", "gofmt" },
+      lua = { "stylua" }
+    },
+    format_on_save = format_on_save_config
+  })
+
+  vim.keymap.set({ "n", "v" }, "<leader>l", function()
+    conform.format(format_on_save_config)
+  end, { desc = "Format file or range (in visual mode)" })
+end
--- lua/adrone13/packer.lua
+++ lua/adrone13/packer.lua
@@ -1,5 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -11,7 +9,7 @@ return require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- Color Scheme
@@ -24,7 +22,7 @@ return require('packer').startup(function(use)
   })
 
   -- AST parser
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   -- Fast selection of bookmarked files
   use('theprimeagen/harpoon')
   -- Detailed change history
@@ -37,20 +35,20 @@ return require('packer').startup(function(use)
     branch = 'v3.x',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
+      { 'neovim/nvim-lspconfig' },
 
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
 
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lua'},
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lua' },
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
     }
   }
 
@@ -62,39 +60,9 @@ return require('packer').startup(function(use)
     end
   }
 
-  -- Linting
   use({
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local conform = require("conform")
-
-      conform.setup({
-        formatters_by_ft = {
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-          css = { "prettier" },
-          html = { "prettier" },
-          json = { "prettier" },
-          go = { "goimports", "gofmt" },
-          lua = { "stylua" }
-        },
-        format_on_save = {
-          -- I recommend these options. See :help conform.format for details.
-          lsp_fallback = true,
-          timeout_ms = 500,
-          -- async = false, -- If the buffer is modified before the formatter completes, the formatting will be discarded.
-        }
-      })
-
-      vim.keymap.set({ "n", "v" }, "<leader>l", function()
-        conform.format({
-          lsp_fallback = true,
-          timeout_ms = 500,
-          -- async = false, -- If the buffer is modified before the formatter completes, the formatting will be discarded.
-        })
-      end, { desc = "Format file or range (in visual mode)" })
-    end
+    config = ConfigConform
   })
-
 end)
